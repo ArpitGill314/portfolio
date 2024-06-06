e5bcb9d2306241c73f83bf913d4b1538ca288ea9
Arpit Gill <arpitgill314@gmail.com>
2024-06-06 13:30:49 -0400

    First Commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,22 @@
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div className="Home">
+      <div className="Name">
+        <h1>ARPIT GILL</h1>
+        <div className="Socials">
+          <button>
+            <i class="fa-regular fa-file"></i>
+          </button>
+          <button>
+            <i class="fa-brands fa-linkedin-in"></i>
+          </button>
+          <button>
+            <i class="fa-brands fa-github"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
